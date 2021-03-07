7f6cd17884ea341715d3b4d0230eefc2d46c06fb
Wajov <waj19971112@qq.com>
2021-03-07 15:37:04 +0800

    updated

--- test.py
+++ test.py
@@ -9,9 +9,6 @@ if __name__ == "__main__":
     G = models.Generator(3, 3)
     G.load_state_dict(torch.load("output/G_X2Y.pth"))
 
-    transform = transforms.Compose([transforms.ToTensor()])
-    x = transform(Image.open("./picture/1.jpg")).unsqueeze(0)
-    y = G(x)
-    y = y.squeeze(0)
-    y = transforms.ToPILImage()(y)
+    x = transforms.ToTensor()(Image.open("./picture/2.jpg")).unsqueeze(0)
+    y = transforms.ToPILImage()(G(x).squeeze(0))
     y.show()
